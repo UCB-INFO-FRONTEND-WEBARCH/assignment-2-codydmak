ad7309ba8edd584ec86e46420151f38b9f634c5a
codydmak <codymak@ischool.berkeley.edu>
2025-10-25 16:12:43 -0700

    Update Set Up

--- task-manager-react/src/App.jsx
+++ task-manager-react/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TaskList from "./components/TaskList";
+import TaskItem from "./components/TaskItem";
+
 
 function App() {
-  return <h1>Task Manager</h1>
+  return <h1>hello,task manager!</h1>
 }
 
 export default App
