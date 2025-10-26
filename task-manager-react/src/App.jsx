791215e40cf0fc7bc1016ab5236aed307efa3c82
codydmak <codymak@ischool.berkeley.edu>
2025-10-25 18:58:30 -0700

    Update App.jsx

--- task-manager-react/src/App.jsx
+++ task-manager-react/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TaskList from "./components/TaskList";
-import TaskForm from "./components/TaskForm";
-import TaskCounter from "./components/TaskCounter";
 
 function App() {
+  const [count, setCount] = useState(0)
 
-  return <h1>Hello</h1>;
+  return (
+    <h1> hello</h1>
+  )
 }
 
 export default App
