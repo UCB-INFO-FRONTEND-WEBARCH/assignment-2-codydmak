c078f2810a868c13d7a8b4b7acc3641c6b178f17
codydmak <codymak@ischool.berkeley.edu>
2025-10-25 18:40:18 -0700

    update

--- task-manager-react/src/App.jsx
+++ task-manager-react/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import TaskCounter from "./components/TaskCounter";
 
 function App() {
 
